66018e5e35b9d95bb521107b29de875b7e4ce8a6
Skadeven <jshi4@andrew.cmu.edu>
2024-01-22 15:37:30 -0500

    Change data.ts

--- src/posts/data.ts
+++ src/posts/data.ts
@@ -9,36 +9,37 @@ const intFields:string[] = [
 ];
 
 interface Post {
-    [key:string]: string[];
+    [key: string]: any;
 }
 
-interface PostData{
-    pids: number[]
+interface PostData {
+    pids: number[];
     posts: Post[];
     fields: string[];
 }
+
 module.exports = function (Posts:any) {
-    Posts.getPostsFields = async function (pids:number[], fields:string[]): Promise<any[]>{
+    Posts.getPostsFields = async function (pids:number[], fields:string[]): Promise<Post[]>{
         if (!Array.isArray(pids) || !pids.length) {
             return [];
         }
         const keys: string[] = pids.map(pid => `post:${pid}`);
-        const postData: any[] = await db.getObjects(keys, fields);
-        const result: any = await plugins.hooks.fire('filter:post.getFields', {
+        const postData: Post[] = await db.getObjects(keys, fields);
+        const result: PostData = await plugins.hooks.fire('filter:post.getFields', {
             pids: pids,
             posts: postData,
             fields: fields,
         });
-        result.posts.forEach((post:any) => modifyPost(post, fields));
+        result.posts.forEach((post:Post) => modifyPost(post, fields));
         return result.posts;
     };
 
-    Posts.getPostData = async function (pid): Promise<any | null> {
+    Posts.getPostData = async function (pid:number): Promise<Post | null> {
         const posts: any[] = await Posts.getPostsFields([pid], []);
         return posts && posts.length ? posts[0] : null;
     };
 
-    Posts.getPostsData = async function (pids:number[]): Promise<any[]> {
+    Posts.getPostsData = async function (pids:number[]): Promise<Post[]> {
         return await Posts.getPostsFields(pids, []);
     };
 
@@ -56,13 +57,13 @@ module.exports = function (Posts:any) {
         await Posts.setPostFields(pid, { [field]: value });
     };
 
-    Posts.setPostFields = async function (pid: number, data: any): Promise<void> {
+    Posts.setPostFields = async function (pid: number, data: object): Promise<void> {
         await db.setObject(`post:${pid}`, data);
         plugins.hooks.fire('action:post.setFields', { data: { ...data, pid } });
     };
 };
 
-function modifyPost(post: any, fields: string[]) {
+function modifyPost(post: Post, fields: string[]):void {
     if (post) {
         db.parseIntFields(post, intFields, fields);
         if (post.hasOwnProperty('upvotes') && post.hasOwnProperty('downvotes')) {
