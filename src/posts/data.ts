8a1b8b2e3f4c6c7a5d8603ca40c49f80ca192539
Skadeven <jshi4@andrew.cmu.edu>
2024-01-25 18:57:21 -0500

    Update data.ts

--- src/posts/data.ts
+++ src/posts/data.ts
@@ -46,7 +46,7 @@ interface PostsFunctions {
     getPostsFields: (pids: number[], fields: string[]) => Promise<PostObjectNew[]>;
     getPostData: (pid: number) => Promise<PostObjectNew | null>;
     getPostsData: (pids: number[]) => Promise<PostObjectNew[]>;
-    getPostField: (pid: number, field: string) => Promise<boolean | null>;
+    getPostField: (pid: number, field: string) => Promise<number | null>;
     getPostFields: (pid: number, fields: string[]) => Promise<PostObjectNew | null>;
     setPostField: (pid: number, field: string, value: boolean) => Promise<void>;
     setPostFields: (pid: number, data: object) => Promise<void>;
@@ -93,19 +93,29 @@ export = function (Posts: PostsFunctions) {
         return result.posts;
     };
 
-    Posts.getPostData = async function (pid: number): Promise<PostObjectNew | null> {
-        const posts = await Posts.getPostsFields([pid], []);
-        return posts && posts.length ? posts[0] : null;
+    Posts.getPostData = async function (pid: number, callback?:(err:Error | null,
+postData: PostObjectNew | null) => void): Promise<PostObjectNew | null> {
+        if (typeof callback === 'function') {
+            try {
+                const posts = await Posts.getPostsFields([pid], []);
+                const result = posts && posts.length ? posts[0] : null;
+                callback(null, result);
+            } catch (error) {
+                callback(error as Error, null);
+            }
+        } else {
+            const posts = await Posts.getPostsFields([pid], []);
+            return posts && posts.length ? posts[0] : null;
+        }
     };
-
 
     Posts.getPostsData = async function (pids: number[]): Promise<PostObjectNew[]> {
         return Posts.getPostsFields(pids, []);
     };
 
-    Posts.getPostField = async function (pid: number, field: string): Promise<boolean | null> {
+    Posts.getPostField = async function (pid: number, field: string): Promise<number | null> {
         const post: PostObjectNew | null = await Posts.getPostFields(pid, [field]);
-        return (post ? post[field] : null) as boolean|null;
+        return (post ? post[field] : null) as number | null;
     };
 
     Posts.getPostFields = async function (pid: number, fields: string[]): Promise<PostObjectNew | null> {
@@ -113,9 +123,20 @@ export = function (Posts: PostsFunctions) {
         return posts ? posts[0] : null;
     };
 
-    Posts.setPostField = async function (pid: number, field: string, value: boolean): Promise<void> {
-        await Posts.setPostFields(pid, { [field]: value });
+    Posts.setPostField = async function (pid: number, field: string, value: boolean,
+        callback?:(err:Error | null) => void): Promise<void> {
+        if (typeof callback === 'function') {
+            try {
+                await Posts.setPostFields(pid, { [field]: value });
+                callback(null);
+            } catch (error) {
+                callback(error as Error);
+            }
+        } else {
+            await Posts.setPostFields(pid, { [field]: value });
+        }
     };
+
     Posts.setPostFields = async function (pid: number, data: dataObj): Promise<void> {
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
@@ -125,3 +146,4 @@ export = function (Posts: PostsFunctions) {
         await plugins.hooks.fire('action:post.setFields', { data: { ...data, pid } });
     };
 }
+
